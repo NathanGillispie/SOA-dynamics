3808c8b641f65cabed6f59cde0434eafcf251c46
Nathan Gillispie <nathaniel.gillispie@gmail.com>
2023-07-05 12:38:50 -0500

    GENERATE_MULTI.py

--- generate_multi.py
+++ generate_multi.py
@@ -1,9 +1,11 @@
-import string
 import os
 import math
+import sys
+import string
 
-number_of_runs = 10
-number_of_digits = int(math.ceil(math.log10(number_of_runs)))+1 
+number_of_runs = 29
+number_of_digits = 2
+#number_of_digits = int(math.ceil(math.log10(number_of_runs)))+1 
 
 checks_dir = "chk_multi"
 trajectories_dir = "ptj_multi"
@@ -53,6 +55,64 @@ if generate_queue_file:
 	os.system("chmod +x " + queue_file)
 
 #
+# CHECKPOINT FILES
+#
+print("\nCheck files...")
+
+input = os.path.join('checkpoints','3_eq-so2.cdl')
+output = os.path.join(checks_dir,'4.cdl')
+rst_in = input.split('.')[0] + '.rst'
+rst_out_str = output.split('.')[0] + "_"
+
+if (not os.path.exists(input)):
+	if (os.path.exists(rst_in)):
+		os.system("ncdump " + rst_in + " > " + input)
+	else:
+		print("check input files")
+		sys.exit()
+
+position_line = 2281
+velocity_line = 4526
+
+overwrite_chk_files = False
+counter = 0
+for counter in range(number_of_runs):
+	c_string = str(counter+1).zfill(number_of_digits)
+	local_filename = "4_{0}.rst".format(c_string)
+	filename = os.path.join(checks_dir, local_filename)
+	answer = ""
+	if os.path.exists(filename) and (not overwrite_chk_files):
+		while (answer != "y") and (answer != "n"):
+			answer = raw_input("Check file exists. Would you like to overwrite all check files? (y/n)\n")
+		if answer == 'y':
+			overwrite_chk_files = True
+		else:
+			break
+	
+	with open(input, 'r') as textfile:
+		lines = textfile.readlines()
+	
+	velocity_string = str(-float(counter)/10 - 0.1)
+
+	if len(lines) > velocity_line + 3:
+		line1 = lines[velocity_line].split(',')
+		line2 = lines[velocity_line + 1].split(',')
+		line3 = lines[velocity_line + 2].split(',')
+		xyS  = string.join(line1[:2], ',') + ','
+		xyO1 = string.join(line2[:2], ',') + ','
+		xyO2 = string.join(line3[:2], ',') + ','
+		lines[velocity_line]     = xyS + velocity_string + ',\n'
+		lines[velocity_line + 1] = xyO1 + velocity_string + ',\n'
+		lines[velocity_line + 2] = xyO2 + velocity_string + ' ;\n'
+
+	with open(output, 'w') as outfile:
+		outfile.writelines(lines)
+
+	os.system("ncgen -o " + rst_out_str + c_string + ".rst " + output)
+	os.system("rm " + output)
+os.system("rm " + input)
+
+#
 # PBS FILES
 #
 print("\nPBS files...")
@@ -81,5 +141,9 @@ NNODES=`uniq $PBS_NODEFILE | wc -l`
 cd $PBS_O_WORKDIR
 
 """)
-		outfile.write("mpirun -n ${{NCPU}} /opt/amber14/bin/sander.MPI -O -i md_inputs/4_production.in -o {md_out} -p md_inputs/water_so2.prmtop -c {chk_in} -r {chk_out} -x {ptraj}".format(md_out="md_outputs/4.out",chk_in="checkpoints/4.rst",chk_out="checkpoints/4.rst",ptraj="ptraj/4.mdcrd"))
-		
+		chk_in = os.path.join(checks_dir,'4_{0}.rst'.format(c_string))
+		chk_out = os.path.join(checks_dir,'4_{0}.rst'.format(c_string))
+		ptraj_out = os.path.join(trajectories_dir,'4_{0}.mdcrd'.format(c_string))
+		md_out = os.path.join(trajectories_dir, '4_{0}.out'.format(c_string))
+
+		outfile.write("mpirun -n ${{NCPU}} /opt/amber14/bin/sander.MPI -O -i md_inputs/4_production.in -o {md_out} -p md_inputs/water_so2.prmtop -c {chk_in} -r {chk_out} -x {ptraj}".format(md_out=md_out, chk_in=chk_in, chk_out=chk_out, ptraj=ptraj_out))
